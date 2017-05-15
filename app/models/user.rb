d7a52e86005e279bfebe8ae1ee25a5955187daba
Julianne Costa <julesleighcosta@gmail.com>
2017-05-15 16:58:46 -0700

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, :email, :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  
 end
