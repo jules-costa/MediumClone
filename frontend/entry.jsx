b2c7e037bc38c673998561fc5863ef41320d09cf
Julianne Costa <julesleighcosta@gmail.com>
2017-05-16 10:29:34 -0700

    bootstrap current user

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -11,8 +11,15 @@ window.logout = logout;
 window.signup = signup;
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
-  const store = configureStore();
   window.store = store;
   ReactDOM.render(<Root store={ store } />, root);
 });
