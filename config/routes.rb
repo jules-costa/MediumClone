1fbd7e7e205f70dd2c5cb23910310379b9caca55
Julianne Costa <julesleighcosta@gmail.com>
2017-05-15 17:13:30 -0700

    Add default format json to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/new'
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:new, :create]
+    resource :session, only: [:new, :create, :destroy]
   end
-
-  namespace :api do
-    get 'sessions/create'
-  end
-
-  namespace :api do
-    get 'sessions/destroy'
-  end
-
-  namespace :api do
-    get 'users/new'
-  end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  get 'users/new'
-
-  get 'users/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
