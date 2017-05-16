16f0ada64b0efd3e2a9a3226e8432d5c8288b002
Julianne Costa <julesleighcosta@gmail.com>
2017-05-16 09:55:27 -0700

    Add store to window. Session state is proper

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import { login, logout, signup } from './util/session_api_util';
 
@@ -9,5 +10,7 @@ window.signup = signup;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Welcome to Ouija</h1>, root);
 });
