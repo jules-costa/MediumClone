472e865be63dc388e1672cedbf51c4f214d10aaf
Julianne Costa <julesleighcosta@gmail.com>
2017-05-15 17:11:24 -0700

    Add API Users and Sessions Controllers and edit routes file to next under namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/new'
+  end
+
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  namespace :api do
+    get 'sessions/destroy'
+  end
+
+  namespace :api do
+    get 'users/new'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
