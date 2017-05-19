c0fe72fa5ef82e64600048b00c6ab836ba452b92
Julianne Costa <julesleighcosta@gmail.com>
2017-05-19 08:23:13 -0700

    logout button styled

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { compose } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from '../middleware/thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
   )
 );
 
