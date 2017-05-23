08c93790fd58b9492bfd6603e46d8124f233be6f
Julianne Costa <julesleighcosta@gmail.com>
2017-05-22 17:38:10 -0700

    comments have edit and delete links that do not work

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { updateComment, destroyComment } from '../../actions/story_actions';
 
-const CommentItem = ({ comment }) => (
+// const handleEdit = (comment) => {
+//   updateComment(comment);
+// };
+//
+// const handleDelete = (id) => {
+//   destroyComment(id);
+// };
+
+const CommentItem = ({ comment, currentUser }) => (
   <section className="comment-container">
     <section className="comment-author-details">
       <img className="comment-author-image" src={comment.author_image}></img>
@@ -8,6 +18,10 @@ const CommentItem = ({ comment }) => (
     </section>
     <div>
       <h5 className="comment-body">{comment.body}</h5>
+    </div>
+    <div className="alter-links">
+      <Link to={`/api/comments/${comment.id}`} className="edit-comment">Edit</Link>
+      <Link to={`/api/comments/${comment.id}`} className="delete-comment">Delete</Link>
     </div>
   </section>
 );
