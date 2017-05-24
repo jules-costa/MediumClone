d3b87f41ff65a7795b0516e072c698902981658d
Julianne Costa <julesleighcosta@gmail.com>
2017-05-24 10:21:12 -0700

    default new user image

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,9 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.image_url = ""
+      @user.image_url = "http://res.cloudinary.com/jules-costa/image/upload/v1495413134/user-2ef10cd87167a7021991eaaf0fb688ea1fda37f520b069b2b523d3a8e399473d_jphioe.png"
+    end
     if @user.save
       login(@user)
       render '/api/users/show'
