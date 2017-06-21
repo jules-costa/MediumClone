2586063af55e4fbe52a29c75c5bc7a2a18922e50
Julianne Costa <julesleighcosta@gmail.com>
2017-06-21 11:35:46 -0700

    exclude redux-logger from store file

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,11 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from '../middleware/thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   )
 );
 
