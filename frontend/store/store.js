47be434eddc9e7a0a749c127bc9870ef70a0e903
Julianne Costa <julesleighcosta@gmail.com>
2017-06-21 11:34:50 -0700

    disable redux devtools in store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import { compose } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from '../middleware/thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   )
 );
 
